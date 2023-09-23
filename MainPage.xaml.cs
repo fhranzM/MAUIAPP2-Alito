1503c177d1919f7611a3f9bee7722988fd21b689
FhranzAlito <126442131+fhranzM@users.noreply.github.com>
2023-09-23 17:59:15 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MAUIApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        String a = "Reality";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnImageButtonaClicked(object sender, EventArgs e)
+        {
+            Expect1.Source = "Real1.png";
+            Lbl1.Text = a;
+        }
+
+        private void OnImageButtonbClicked(object sender, EventArgs e)
+        {
+            Expect2.Source = "Real2.png";
+            Lbl2.Text = a;
+        }
+
+    }
+}
